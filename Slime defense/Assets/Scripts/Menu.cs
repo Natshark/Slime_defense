cbadc4a39fb84af074337a442904235cdca82a22
VitaCode <131052261+VitaANDCode@users.noreply.github.com>
2023-06-22 17:48:30 +0300

    Menu How To Play

--- Slime defense/Assets/Scripts/Menu.cs
+++ Slime defense/Assets/Scripts/Menu.cs
@@ -3,6 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public int counterSlides = 0;
+    public GameObject[] slides;
+    public GameObject howToPlayUI;
+
     void Start()
     {
         Time.timeScale = 1.0f;
@@ -11,6 +15,27 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+
+    public void howToPlay() 
+    {
+        howToPlayUI.SetActive(true);
+        slides[counterSlides].SetActive(true);
+    }
+
+    public void nextSlide()
+    {
+        counterSlides++;
+        slides[counterSlides].SetActive(true);
+        slides[counterSlides-1].SetActive(false);
+    }
+
+    public void toMenu()
+    {
+        slides[counterSlides].SetActive(false);
+        howToPlayUI.SetActive(false);
+        counterSlides = 0;
+    }
+
     public void Exit() 
     {
         Application.Quit();
